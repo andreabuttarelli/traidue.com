e53e98245da8a3e86fbcb6413821acc21e9a3535
Andrea Buttarelli <49411143+andreabuttarelli@users.noreply.github.com>
2026-03-02 13:27:41 +0100

    Update +page.server.ts

--- src/routes/wiki/+page.server.ts
+++ src/routes/wiki/+page.server.ts
@@ -3,10 +3,19 @@ import { getAllQuizzes } from '$lib/utils/quiz';
 
 export const prerender = false;
 
+function shuffle<T>(array: T[]): T[] {
+	const shuffled = [...array];
+	for (let i = shuffled.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+	}
+	return shuffled;
+}
+
 export function load() {
 	const articles = getAllArticles();
 	const quizzes = getAllQuizzes();
 	const categories = [...new Set(articles.map((a) => a.category))];
 	const tags = [...new Set(articles.flatMap((a) => a.tags))];
-	return { articles, quizzes, categories, tags };
+	return { articles: shuffle(articles), quizzes, categories, tags };
 }
